2b8e1c1575dbfd31a2e1d4457ab92fb9ee0c3127
Samuel ATAKE <samuelatake1881@gmail.com>
2022-04-03 03:40:33 +0000

    correct url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://lktransportbackend.herokuapp.com/',
+  baseURL: 'https://lktransportbackend.herokuapp.com',
   // baseURL: 'http://localhost:8080/',
   headers: {
     'Content-type': 'application/json'
